1ba90e18defdcb23dc60543d00a26b513126129e
Jackycyc <Jackychiu02@gmail.com>
2023-02-04 03:55:37 +0800

    updated nextday sprite

--- Assets/Script/NextDay_Btn.cs
+++ Assets/Script/NextDay_Btn.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NextDay_Btn : MonoBehaviour
 {
     public GameManager instance;
 
     public void Update(){
-        if(instance.state == GameState.Event){
-            this.gameObject.SetActive(false);
+        if(instance.state == GameState.Event && this.gameObject.activeSelf){
+            StartCoroutine(DisableItself());
         }
+    }
+
+    IEnumerator DisableItself(){
+        this.GetComponent<Button>().enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        this.gameObject.SetActive(false);
     }
 
     public void ClickedNextDay()
